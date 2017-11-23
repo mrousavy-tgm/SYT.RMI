b447940dd498b94805c2d23083d789529c3d698f
Marc Rousavy <marcrousavy@hotmail.com>
2017-11-23 10:56:16 +0100

    Implement Logger interface for System.out strings

---
+++ src/JLogger.java
@@ -0,0 +1,24 @@
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class JLogger implements Logger<String> {
+    private PrintStream stream;
+
+    public JLogger(PrintStream stream) {
+        if (stream == null){
+            this.stream = System.out;
+        } else{
+            this.stream = stream;
+        }
+    }
+
+    @Override
+    public void Log(Severity severity, String message) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss:ms");
+        LocalDateTime now = LocalDateTime.now();
+
+        String buffer = String.format("[%s] [%s]: %s", now.toString(), severity.toString(), message);
+        stream.println(buffer);
+    }
+}
