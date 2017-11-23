6c9a64d052307957cc7d0785b0aa89079dacfe7a
Marc Rousavy <marcrousavy@hotmail.com>
2017-11-23 11:12:58 +0100

    Use Logger in Main Server

--- src/server/Main.java
+++ src/server/Main.java
@@ -1,28 +1,38 @@
 package server;
 
+import utils.JLogger;
+import utils.Logger;
+
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 public class Main {
 
     public static void main(String[] args) {
-        ComputeEngine engine = new ComputeEngine();
+        Logger<String> logger = new JLogger(System.out);
+
         int port = 0;
         String name = "Compute";
+        ComputeEngine engine = new ComputeEngine(name, port);
 
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
         try {
-            engine.start(name, port);
-            System.out.println("Compute Engine successfully bound to port " + port + ", with name \"" + name + "\"");
+            engine.start();
+            logger.Log(Logger.Severity.Info, "Compute Engine successfully bound to port " + port + ", with name \"" + name + "\"");
+            engine.stop();
         } catch (SecurityManagerException ex) {
-            System.out.println("Invalid Security Manager! " + ex.getMessage());
+            logger.Log(Logger.Severity.Error, "Invalid Security Manager! " + ex.getMessage());
             ex.printStackTrace();
         } catch (IllegalArgumentException ex) {
-            System.out.println("Illegal Argument specified! " + ex.getMessage());
+            logger.Log(Logger.Severity.Error, "Illegal Argument specified! " + ex.getMessage());
             ex.printStackTrace();
         } catch (RemoteException ex) {
-            System.out.println("Java RMI Remote Exception! " + ex.getMessage());
+            logger.Log(Logger.Severity.Error, "Java RMI Remote Exception! " + ex.getMessage());
+            ex.printStackTrace();
+        } catch (NotBoundException ex) {
+            logger.Log(Logger.Severity.Error, "Engine was not bound, stopping failed! " + ex.getMessage());
             ex.printStackTrace();
         }
     }
