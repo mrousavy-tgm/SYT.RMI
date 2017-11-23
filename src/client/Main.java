8124251ae66e9492e5156b57cc0d62ee0681d7bb
Marc Rousavy <marcrousavy@hotmail.com>
2017-11-23 11:05:10 +0100

    Implement seperate main func

---
+++ src/client/Main.java
@@ -0,0 +1,44 @@
+package client;
+
+import compute.Compute;
+import utils.JLogger;
+import utils.Logger;
+
+import java.math.BigDecimal;
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger<String> logger = new JLogger(System.out);
+
+        String name = "Compute";
+        int port = 0;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (Exception ex) {
+
+        }
+
+        try {
+            Registry registry = LocateRegistry.getRegistry(args[0]);
+            Compute comp = (Compute) registry.lookup(name);
+            Fibonacci task = new Fibonacci(Integer.parseInt(args[1]));
+            BigDecimal fib = comp.executeTask(task);
+            logger.Log(Logger.Severity.Info, "Fibonacci calculated!: " + fib);
+            logger.Log(Logger.Severity.Info, fib.toString());
+        } catch (AccessException e) {
+            logger.Log(Logger.Severity.Error, "Invalid Access! " + e.getMessage());
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            logger.Log(Logger.Severity.Error, "Failed to retrieve remote info! " + e.getMessage());
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            logger.Log(Logger.Severity.Error, "Specified port is not bound! " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
