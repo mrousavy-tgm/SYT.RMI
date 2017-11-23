58d54e6c7c52e8e589cb8260e9140860787b09c5
Marc Rousavy <marcrousavy@hotmail.com>
2017-11-23 11:17:02 +0100

    Switch between Pi and Fib in Main

--- src/client/Main.java
+++ src/client/Main.java
@@ -12,19 +12,46 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Main {
+
+    private enum ComputeType {
+        Pi,
+        Fibonacci
+    }
+
+    private static BigDecimal calcFibonacci(Compute compute, int count) throws RemoteException {
+        Fibonacci task = new Fibonacci(count);
+        return compute.executeTask(task);
+    }
+    private static BigDecimal calcPi(Compute compute, int digits) throws RemoteException {
+        Pi task = new Pi(digits);
+        return compute.executeTask(task);
+    }
+
     public static void main(String[] args) {
         Logger<String> logger = new JLogger(System.out);
 
         String name = "Compute";
         int port = 1099;
+        ComputeType type = ComputeType.Fibonacci;
 
         try {
             Registry registry = LocateRegistry.getRegistry(port);
             Compute comp = (Compute) registry.lookup(name);
-            Fibonacci task = new Fibonacci(Integer.parseInt(args[1]));
-            BigDecimal fib = comp.executeTask(task);
-            logger.Log(Logger.Severity.Info, "Fibonacci calculated!: " + fib);
-            logger.Log(Logger.Severity.Info, fib.toString());
+
+            switch(type) {
+                case Fibonacci:
+                    int count = Integer.parseInt(args[1]);
+                    BigDecimal fib = calcFibonacci(comp, count);
+                    logger.Log(Logger.Severity.Info, "Fibonacci calculated!: " + fib);
+                    logger.Log(Logger.Severity.Info, fib.toString());
+                    break;
+                case Pi:
+                    int digits = Integer.parseInt(args[1]);
+                    BigDecimal pi = calcPi(comp, digits);
+                    logger.Log(Logger.Severity.Info, "Pi calculated!: " + pi);
+                    logger.Log(Logger.Severity.Info, pi.toString());
+                    break;
+            }
         } catch (AccessException e) {
             logger.Log(Logger.Severity.Error, "Invalid Access! " + e.getMessage());
             e.printStackTrace();
