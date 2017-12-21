d87eda526444774a623462a7fa6dbbb68064b7b9
Marc Rousavy <marcrousavy@hotmail.com>
2017-12-21 11:02:37 +0100

    Update LoadBalancer main

--- src/loadbalancer/Main.java
+++ src/loadbalancer/Main.java
@@ -5,7 +5,9 @@ import utils.JLogger;
 import utils.Logger;
 
 public class Main {
+
     public static void main(String[] args) {
+        int START_PORT = 2099;
         Logger<String> logger = new JLogger(System.out);
         Proxy balancer = new LoadBalancer();
         if (System.getSecurityManager() == null) {
@@ -14,7 +16,7 @@ public class Main {
 
         for (int i = 0; i < 4; i++) {
             String name = "Compute" + i;
-            int port = 1099 + i;
+            int port = START_PORT + i;
             ComputeServer server = new ComputeServer(name, port); // Create a new server
             logger.Log(Logger.Severity.Info, "Created Server in registry with name \"" + name + "\" at " + port);
 
@@ -33,19 +35,16 @@ public class Main {
             }
         }
 
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        for (int i = 0; i < 4; i++) {
-            ComputeServer server = (ComputeServer) balancer.list.get(i);
-            try {
-                server.stop();
-            } catch(Exception ex) {
-                logger.Log(Logger.Severity.Error, "Cannot stop server \"" + server.getName() + "\"! " + ex.getMessage());
+        // Hook SIGTERM or any shutdown event so the sockets get closed
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            for (int i = 0; i < 4; i++) {
+                ComputeServer server = (ComputeServer) balancer.list.get(i);
+                try {
+                    server.stop();  // Stop the servers again
+                } catch(Exception ex) {
+                    logger.Log(Logger.Severity.Error, "Cannot stop server \"" + server.getName() + "\"! " + ex.getMessage());
+                }
             }
-        }
+        }));
     }
 }
